de908420a63a04710802ca2c0e2e9e6ef563f707
Carlo Eugster <carlo@relaun.ch>
2021-07-05 08:27:45 +0200

    Updated argparser

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -9,11 +8,9 @@ let package = Package(
       .macOS(.v10_14),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.1"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "duotone",
             dependencies: [
