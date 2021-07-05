29420046198bd0a664e6cf9772adb9d4a6be4a24
Carlo Eugster <carlo@relaun.ch>
2021-07-05 20:23:09 +0200

    Added list command

--- Sources/duotone/main.swift
+++ Sources/duotone/main.swift
@@ -14,7 +14,7 @@ private let validImageExtensions = ["jpg", "jpeg", "png"]
 struct Duotone: ParsableCommand {
     static var configuration = CommandConfiguration(
         abstract: "A utility for duotoning images.",
-        subcommands: [Process.self, Add.self, Remove.self],
+        subcommands: [Process.self, Add.self, Remove.self, List.self],
         defaultSubcommand: Process.self)
 }
 
