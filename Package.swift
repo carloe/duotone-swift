f8fe43f27d854c65763c0d4590e1a1fe2037c8b6
Carlo Eugster <carlo@relaun.ch>
2022-11-11 09:36:01 +0100

    Bump tool version and argparser version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.7
 
 import PackageDescription
 
@@ -8,11 +8,11 @@ let package = Package(
       .macOS(.v10_14)
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.1"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
         .package(url: "https://github.com/JohnSundell/Files", from: "4.2.0")
     ],
     targets: [
-        .target(
+        .executableTarget(
             name: "duotone",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
