9e515cb7c4dfba9e9d50857a23f9ab1524bdca31
Carlo Eugster <carlo@relaun.ch>
2026-05-05 23:56:36 +0200

    Bump version to 1.1.1

--- Sources/duotone/Duotone.swift
+++ Sources/duotone/Duotone.swift
@@ -13,7 +13,7 @@ struct DuotoneError: Error, CustomStringConvertible {
 struct Duotone: ParsableCommand {
     static let configuration = CommandConfiguration(
         abstract: "A utility for duotoning images.",
-        version: "1.1.0",
+        version: "1.1.1",
         subcommands: [Process.self, Add.self, Remove.self, List.self],
         defaultSubcommand: Process.self)
 }
